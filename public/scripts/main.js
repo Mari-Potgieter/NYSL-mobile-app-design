5976e6518cbd9e0e5854cb93379917d58603b3a9
Mari Potgieter <potgieter_mari@yahoo.com>
2018-06-29 15:07:56 +0200

    portrait and landscape view

---
+++ public/scripts/main.js
@@ -0,0 +1,357 @@
+//    {
+//        "gamesObject": [
+//            {
+//                "date": "1 September",
+//                "team_1": "U1",
+//                "team_2": "U4",
+//                "location": "AJ Katzenmaier",
+//                "time": "09:30am",
+//
+//
+//        },
+//            {
+//                "date": "1 September",
+//                "team_1": "U3",
+//                "team_2": "U2",
+//                "location": "Greenbay",
+//                "time": "13:00pm",
+//
+//        },
+//            {
+//                "date": "8 September",
+//                "team_1": "U5",
+//                "team_2": "U6",
+//                "location": "Howard A Yeager",
+//                "time": "09:30am",
+//
+//
+//            },
+//            {
+//                "date": "8 September",
+//                "team_1": "U6",
+//                "team_2": "U1",
+//                "location": "Marjorie P Hart",
+//                "time": "13:00pm",
+//            },
+//            {
+//                "date": "15 September",
+//                "team_1": "U2",
+//                "team_2": "U4",
+//                "location": "North Elementary",
+//                "time": "09:30am",
+//            },
+//            {
+//                "date": "15 September",
+//                "team_1": "U3",
+//                "team_2": "U5",
+//                "location": "AJ Katzenmaier",
+//                "time": "13:00pm",
+//            },
+//            {
+//                "date": "22 September",
+//                "team_1": "U1",
+//                "team_2": "U3",
+//                "location": "South Elementary",
+//                "time": "09:30am",
+//            },
+//            {
+//                "date": "22 September",
+//                "team_1": "U2",
+//                "team_2": "U6",
+//                "location": "Howard A Yeager",
+//                "time": "13:00pm",
+//            },
+//            {
+//                "date": "6 October",
+//                "team_1": "U2",
+//                "team_2": "U5",
+//                "location": "Marjorie P Hart",
+//                "time": "09:30am",
+//            },
+//            {
+//                "date": "6 October",
+//                "team_1": "U1",
+//                "team_2": "U6",
+//                "location": "South Elementary",
+//                "time": "13:00pm",
+//            },
+//            {
+//                "date": "8 October",
+//                "team_1": "U3",
+//                "team_2": "U4",
+//                "location": "Howard A Yeager",
+//                "time": "09:30am",
+//            },
+//             {
+//                "date": "8 October",
+//                "team_1": "U5",
+//                "team_2": "U1",
+//                "location": "Greenbay",
+//                "time": "13:00pm",
+//            },
+//            {
+//                "date": "20 October",
+//                "team_1": "U6",
+//                "team_2": "U3",
+//                "location": "North Elementary",
+//                "time": "09:30am",
+//            },
+//            {
+//                "date": "20 October",
+//                "team_1": "U2",
+//                "team_2": "U4",
+//                "location": "Marjorie P Hart",
+//                "time": "13:00pm",
+//            },
+//            {
+//                "date": "27 October",
+//                "team_1": "U3",
+//                "team_2": "U1",
+//                "location": "AJ Katzenmaier",
+//                "time": "09:30am",
+//            },
+//            {
+//                "date": "27 October",
+//                "team_1": "U5",
+//                "team_2": "U6",
+//                "location": "Howard A Yeager",
+//                "time": "13:00pm",
+//            },
+//
+//
+//],
+//        "teams": ["U1", "U2", "U3", "U4", "U5", "U6"],
+//        "locations": {
+//            "AJ Katzenmaier": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2949.7810520615812!2d-87.86492878519806!3d42.32586847918923!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880f932fbc6ba7cd%3A0xcf2bbe275c6da815!2sA+J+Katzenmaier+Elementary+School!5e0!3m2!1snl!2snl!4v1529927888834",
+//
+//            "Greenbay": "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5938.045933529444!2d-87.637833!3d41.913865!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb351205fae50c6f3!2sGreenbay+Elementary!5e0!3m2!1sen!2snl!4v1526241780920",
+//
+//            "Howard A Yeager": "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5937.17169922377!2d-87.662921!3d41.923261!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb6cb22b2f0358874!2sHoward+A+Yeager+Elementary!5e0!3m2!1sen!2snl!4v1526241835076",
+//
+//            "Marjorie P Hart": "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5936.579667457594!2d-87.645973!3d41.929623!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xdbff5d5dfcfcfa35!2sMarjorie+P+Hart+Elementary!5e0!3m2!1sen!2snl!4v1526241859697",
+//
+//            "North Elementary": "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1484.667771542992!2d-87.646123!3d41.90714500000001!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5736e62f19086c62!2sNorth+Elementary!5e0!3m2!1sen!2snl!4v1526241923671",
+//
+//            "South Elementary": "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d371.09350416239585!2d-87.651369!3d41.919775!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd3196fb41dc7%3A0x970be7f7d6336df5!2s2101+N+Fremont+St%2C+Chicago%2C+IL+60614%2C+USA!5e0!3m2!1sen!2snl!4v1526241950651"
+//        },
+//
+//
+//    }
+
+
+var main = new Vue({
+    el: '#main',
+    data: {
+        games: [],
+        dates: [],
+        teams: [],
+        dateHeading: [],
+        teamHeading: [],
+        locationHeading: [],
+        locations: [],
+        locationNames: [],
+        maps: [],
+        team_data: [],
+        path: 0,
+        history: [0],
+
+
+
+    },
+    created: function () {
+        this.start();
+
+
+    },
+    methods: {
+        start: function () {
+            var fetchConfig =
+                fetch("https://api.myjson.com/bins/jext2", {
+                    method: "GET",
+                }).then(function (response) {
+                    if (response.ok) {
+                        return response.json();
+                    }
+
+                }).then(function (json) {
+
+                    var data = json;
+                    console.log("data", data);
+
+                    main.games = data.gamesObject;
+                    main.teams = data.teams;
+                    main.locations = data.locations;
+                    //                    console.log(main.locations);
+                    //                    console.log(main.games);
+                    main.findDates();
+                    //                    main.seeAllPage();
+                    //                        main.removeDuplicates(main.games);
+                    main.findTheLocations();
+
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+        },
+        backToHomePage: function () {
+          this.path = 0;  
+        },
+        backToPreviousPage: function () {
+            //            if (this.path == 1) {
+            //                this.path = 0
+            //            }
+            //            else if (this.path == 2) {
+            //                this.path = 1
+            //            }
+            //            else if (this.path == 3) {
+            //                this.path = 2
+            //            }
+            //            else if (this.path == 4) {
+            //                this.path = 3
+            //            }
+            var history = this.history;
+            console.log(history);
+            if (history.length > 1) {
+                var lastPage = history[history.length - 2];
+                history.splice(-1, 1);
+                this.path = lastPage;
+            }
+            //            if (index < 1) {
+            //                index = 2;
+            //            }
+            //            console.log(index-1)
+
+            console.log(this.path);
+
+
+
+        },
+        mainToCalendar: function () {
+            this.path = 1;
+            this.history.push(this.path);
+
+        },
+        seeAllDates: function () {
+            this.path = 2;
+            this.history.push(this.path);
+
+        },
+        seeOneDate: function (date) {
+            this.path = 3;
+            var dateHeading = this.dateHeading;
+            var x = document.querySelector("[data-name='" + date + "']")
+            dateHeading = x.dataset.name;
+            this.dateHeading = dateHeading;
+            console.log(this.dateHeading);
+
+            var games = main.games;
+            var index = [];
+            for (var i = 0; i < games.length; i++) {
+
+                if (games[i].date == this.dateHeading) {
+                    index.push(i);
+                    console.log(index);
+                }
+
+            }
+
+            this.team_data = [];
+            for (var i = 0; i < index.length; i++) {
+                this.team_data.push(games[index[i]]);
+            }
+            console.log(this.team_data);
+            this.history.push(this.path);
+        },
+        displayPages: function (team) {
+            this.path = 7;
+
+            var teamHeading = this.teamHeading;
+            var x = document.querySelector("[data-name='" + team + "']")
+            teamHeading = x.dataset.name;
+            this.teamHeading = teamHeading;
+
+            var games = main.games;
+            var index = [];
+            for (var i = 0; i < games.length; i++) {
+
+                if (games[i].team_1 == this.teamHeading || games[i].team_2 == this.teamHeading) {
+                    index.push(i);
+                    console.log(index);
+                }
+
+            }
+
+            this.team_data = [];
+            for (var i = 0; i < index.length; i++) {
+                this.team_data.push(games[index[i]]);
+            }
+            console.log(this.team_data);
+            this.history.push(this.path);
+        },
+        showPages: function (location) {
+            this.path = 8;
+            var locationHeading = this.locationHeading;
+            var x = document.querySelector("[data-name='" + location + "']")
+            locationHeading = x.dataset.name;
+            this.locationHeading = locationHeading;
+            var locations = this.locations;
+
+            for (var location in locations) {
+                if (location == this.locationHeading)
+                    this.maps = (locations[location])
+
+            }
+            console.log(this.maps);
+            this.history.push(this.path);
+        },
+        mainToTeams: function () {
+            this.path = 4;
+            this.history.push(this.path);
+        },
+        mainToLocation: function () {
+            this.path = 5;
+            this.history.push(this.path);
+        },
+        mainToChat: function () {
+            this.path = 6;
+            this.history.push(this.path);
+        },
+        findDates: function () {
+            var games = this.games;
+            var array = [];
+            for (h = 0; h < games.length; h++) {
+                if ((2 * h + 1) < games.length) {
+
+                    array.push(games[2 * h + 1].date);
+                }
+            }
+            //            console.log(main.allDates);
+            this.dates = array;
+        },
+        findTheLocations: function () {
+            var empty = [];
+            var locations = this.locations;
+            for (var location in locations) {
+                empty.push(location);
+            }
+            //            console.log(empty);
+            var locationNames = this.locationNames;
+            locationNames = empty;
+            this.locationNames = locationNames;
+
+        },
+        
+
+        //            removeDuplicates: function (array) {
+        //                //            let teams = [];
+        //                for (let i = 0; i < array.length; i++) {
+        //                    if (this.teams.indexOf(array[i].team_1) == -1) {
+        //                        this.teams.push(array[i].team_1);
+        //                    }
+        //                }
+        //                console.log(this.teams);
+        //                //            this.teams.push(unique_array);
+        //            }
+    },
+
+})
